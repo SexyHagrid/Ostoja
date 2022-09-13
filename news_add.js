401a0d6f316a5d0cb798c5db8fb0d47df57ba988
ppatyk <78248344+ppatyk@users.noreply.github.com>
2022-09-13 13:49:17 +0200

    file improved

---
+++ news_add.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  var data = {};
+  var addNewsButton = $('#createButton');
+  addNewsButton.on('click', function() {
+    createNews();
+  });
+
+  function createNews() {
+    xhttp = new XMLHttpRequest();
+    var resolutionID = document.getElementById('resolutionID').value;
+    var resolutionText = document.getElementById('resolutionText').value;
+    var resolutionImage = document.getElementById('resolutionImage').value;
+
+    var filepath = "/assets/aktualnosci_pliki/" + resolutionImage;
+
+
+    var request = 'news_add.php?id=' + resolutionID + '&text=' + resolutionText + '&image=' + filepath;
+
+    
+
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.responseText.includes("SUCCESS")) {
+          alert('Aktualnosc została dodana');
+          document.location.href = "news.php";
+        } else {
+          alert('Błąd podczas dodawania aktualnosci');
+        }
+      }
+    }
+
+    xhttp.open('GET', request, true);
+    xhttp.send();
+  }
+
+  function isAdminOrEditor() {
+    return data.userRole == 1 || data.userRole == 2;
+  }
+
+  function getSessionID() {
+    return localStorage.getItem('sessionID');
+  }
+});
